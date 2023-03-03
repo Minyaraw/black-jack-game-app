965101046ed75b0670bfef14aea9d98996758d4e
Minyaraw <120593004+Minyaraw@users.noreply.github.com>
2023-03-02 23:30:47 -0500

    updates

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
-
-
-let firstCard = 6
-let secondCard = 9
-
+let firstCard = 10
+let secondCard = 11
 let sum = firstCard + secondCard
+let hasBlackJack = false
+let isAlive = true
+let message = ""
 
-console.log(sum)
+function startGame() {
+
+    if (sum <= 20) {
+        message = "Do you want to draw a new card? 🙂"
+    } else if (sum === 21) {
+        message = "Wohoo! You've got Blackjack! 🥳"
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game! 😭"
+        isAlive = false
+}
+}
