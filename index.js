cfdcdbaa6c574507af36dd10d38469541e793f17
Minyaraw <120593004+Minyaraw@users.noreply.github.com>
2023-03-07 23:43:31 -0500

    updates

--- index.js
+++ index.js
@@ -1,20 +1,40 @@
-let firstCard = getRandomCard()
-let secondCard = getRandomCard()
-let cards = [firstCard, secondCard]
-let sum = firstCard + secondCard
+let player = {
+    name: "Minyara",
+    chips: 200
+
+    
+}
+
+let cards = []
+let sum = 0
 let hasBlackJack = false
-let isAlive = true
+let isAlive = false
 let message = ""
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
+let playerEl = document.getElementById("player-el")
+
+playerEl.textContent = player.name + ": $" + player.chips
 
 function getRandomCard() {
-    return Math.floor( Math.random()*13 ) + 1 
+    let randomNumber = Math.floor( Math.random()*13 ) + 1 
+    if (randomNumer > 10) {
+        return 10
+    } else if (randomNumer === 1) {
+        return 11
+    } else {
+        return randomNumer
+    }
 }
 
 
 function startGame() {
+    isAlive = true
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard
     renderGame()
 }
 
@@ -38,8 +58,10 @@ messageEl.textContent = message
 }
 
 function newCard() {
-    let card = getRandomCard()
-    sum += card 
-    cards.push(card)
-    renderGame()
+    if (isAlive === true && hasBlackJack === false) {
+        let card = getRandomCard()
+        sum += card
+        cards.push(card)
+        renderGame()        
+    }
 }
